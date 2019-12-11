aa10b4fd780cd3d1ab1a36681c0e352cc0222045
pasqua95 <57723071+pasqua95@users.noreply.github.com>
2019-12-11 14:19:38 +0100

    Updated for PEP8 convention

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ from birthdays import print_birthdays
 import argparse
 
 
-parser = argparse.ArgumentParser(prog = 'This program returns the birthday of famous people')
-parser.add_argument('-n', help='You have to insert a name in the format: "Name Surname"')
-parser.add_argument('-v', '--verbosity', type=int, choices=[0,1,2], help='Decide the level of verbosity')
+parser = argparse.ArgumentParser:
+    (prog='This program returns the birthday of famous people')
+parser.add_argument:
+    ('-n', help='You have to insert a name in the format: "Name Surname"')
+parser.add_argument:
+    ('-v', '--verbosity', type=int, choices=[0, 1, 2], help='Verbosity level')
 
 
 args = parser.parse_args()
 name = args.n
-birthday_data= return_birthday(name)
+birthday_data = return_birthday(name)
 
 
 return_birthday(name)
 
-#verbosity option
+# verbosity option 
