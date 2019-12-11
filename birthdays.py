16e53a53cea25eeb7789a0d012cd3a8eb450456d
pasqua95 <57723071+pasqua95@users.noreply.github.com>
2019-12-11 14:46:21 +0100

    Updated birthdays with CSV reader code

--- birthdays.py
+++ birthdays.py
@@ -3,6 +3,15 @@ the dates of birth of 5 famous people and their names. The dictionary has as
 keys the names and as values the dates of birth. The functions operate on this  
 dictionary"""
 
+#imort .csv file with data list#
+import csv
+
+reader = csv.reader (open('databirth2.csv', 'r'))
+d = {}
+for row in reader:
+    k,v = row  
+    d[k]= v
+
 
 """The first function prints all the available names in the dictionary before a 
 wellcoming phrase"""
@@ -10,8 +19,8 @@ wellcoming phrase"""
 
 def print_birthdays():
     print('Welcome to the dictionary.We know the birthdays of these people:')
-    for name in birthdays:
-        print(name)
+     for e in d:
+        print(e)
 
 """The second funtion iterats trough the dictionary and prints, if the name is 
 in birthdays, the name requested along with the date of birth of that person. 
@@ -20,8 +29,9 @@ not in the dictionary"""
 
 
 def return_birthday(name):
-    if name in birthdays:
-        print('{}\'s birthday is {}.'.format(name, birthdays[name]))
+    if name in d:
+        return d[name]
     else:
-        print('Sadly, we don\'t have {}\'s birthday.'.format(name))
+        return False 
 
+     
