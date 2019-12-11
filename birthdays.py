7dbc57a627c2945fb6fb6b67259ab7b0eebb99e0
pasqua95 <57723071+pasqua95@users.noreply.github.com>
2019-12-11 14:04:00 +0100

    Deleted birthdays list. Added to cdv file.

--- birthdays.py
+++ birthdays.py
@@ -1,11 +1,5 @@
 #This code contains two functions and a dictionary "birthdays", that contains the dates of birth of 5 famous people and their names. The dictionary has as keys the names and as values the dates of birth. The functions operate on this dictionary#
 
-birthdays = {
-    'Albert Einstein': '03/14/1879',
-    'Benjamin Franklin': '01/17/1706',
-    'Ada Lovelace': '12/10/1815',
-    'Donald Trump': '06/14/1946',
-    'Rowan Atkinson': '01/6/1955'}
 
 #The first function prints all the available names in the dictionary before a wellcoming phrase#
 def print_birthdays():
