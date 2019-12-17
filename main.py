77c36d9c6fb9a7544a5d594b51bbcfc5045eecbb
pasqua95 <marcopasqua95@gmail.com>
2019-12-17 14:58:38 +0100

    correct main.py

--- main.py
+++ main.py
@@ -4,19 +4,17 @@ from birthdays import print_birthdays
 import argparse
 
 
-parser = argparse.ArgumentParser:
-    (prog='This program returns the birthday of famous people')
-parser.add_argument:
-    ('-n', help='You have to insert a name in the format: "Name Surname"')
-parser.add_argument:
-    ('-v', '--verbosity', type=int, choices=[0, 1, 2], help='Verbosity level')
+print_birthdays()
+
+
+parser = argparse.ArgumentParser(prog='This program returns the birthday of famous people')
+parser.add_argument('n', help='You have to insert a name in the format: "Name Surname"')
+parser.add_argument('-v', '--verbosity', type=int, choices=[0, 1, 2], help='Verbosity level')
 
 
 args = parser.parse_args()
 name = args.n
-birthday_data = return_birthday(name)
 
 
 return_birthday(name)
-
 # verbosity option 
