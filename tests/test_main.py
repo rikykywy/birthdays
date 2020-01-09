d740a4b82f72cc5b279b7fda0416ba96b2eadce0
pasqua95 <57723071+pasqua95@users.noreply.github.com>
2020-01-09 13:40:20 +0100

    Create test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,33 @@
+import unittest
+from datapackage.test import csv_reader
+from datapackage.test import type_f
+import sys
+import os
+
+
+class TestMain(unittest.TestCase):
+
+    def setUp(self):
+        self.temporary_file = "/tmp/temporary_file.csv"
+        f = open(self.temporary_file, 'w')
+        f.close()
+
+    def test_no_datafile(self):
+        """Check if there is a csv file."""
+        self.assertFalse(os.path.exists("birth_package/random_name.csv"))
+
+    def test_empty_datafile(self):
+        """Check the presence of data inside the csv file."""
+        datafile = csv_reader(path=self.temporary_file)
+        self.assertFalse(datafile)
+
+    def test_valid_extension(self):
+        """Check the extension of the file."""
+        extension = type_f(path=self.temporary_file)
+        self.assertEqual(extension, ".csv")
+
+    def tearDown(self):
+        os.remove(self.temporary_file)
+
+if __name__ == '__main__':
+    unittest.main()
